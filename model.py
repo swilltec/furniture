f82451fea822502bb87281ca3313d46135321bd0
swill <swilltec@gmail.com>
2021-04-04 14:56:31 -0400

    added Person object

--- model.py
+++ model.py
@@ -1,6 +1,24 @@
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, NamedTuple, NewType
+from collections import namedtuple
 from datetime import date
+
+
+Quantity = NewType("Quantity", int)
+Sku = NewType("Sku", str)
+Reference = NewType("Reference", str)
+Line = namedtuple('Line', ['sku', 'qty'])
+
+
+@dataclass(frozen=True)
+class Name:
+    first_name: str
+    surname: str
+
+
+class Money(NamedTuple):
+    currency: str
+    value: int
 
 
 @dataclass(frozen=True)
@@ -11,15 +29,39 @@ class OrderLine:
 
 
 class Batch:
-    def __init__(self, ref: str, sku: str,
-                 qty: int, eta: Optional[date]):
+    def __init__(self, ref: Reference, sku: Sku,
+                 qty: Quantity, eta: Optional[date]):
         self.reference = ref
         self.sku = sku
         self.eta = eta
-        self.available_quantity = qty
+        self._purchased_quantity = qty
+        self._allocations = set()
 
     def allocate(self, line: OrderLine):
-        self.available_quantity -= line.qty
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+    def deallocate(self, line: OrderLine):
+        if line in self._allocations:
+            self._allocations.remove(line)
+
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.qty for line in self._allocations)
+
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_quantity - self.allocated_quantity
 
     def can_allocate(self, line: OrderLine) -> bool:
-       return self.sku == line.sku and self.available_quantity >= line.qty
+           return self.sku == line.sku and self.available_quantity >= line.qty
+
+    def can_allocate(self, line: OrderLine) -> bool:
+        return self.sku == line.sku and self.available_quantity >= line.qty
+
+
+class Person:
+    def __init__(self, name: Name):
+        self.name = name
+
+
--- test_batches.py
+++ test_batches.py
@@ -1,5 +1,6 @@
 from datetime import date
-from model import Batch, OrderLine
+from model import Batch, OrderLine, \
+    Money, Name, Line, Person
 
 
 def make_batch_and_line(sku, batch_qty, line_qty):
@@ -31,3 +32,29 @@ def test_cannot_allocate_if_skus_do_not_match():
     different_sku_line = OrderLine(
         "order-123", "EXPENSIVE-TOASTER", 10)
     assert batch.can_allocate(different_sku_line) is False
+
+
+def test_can_only_deallocate_allocated_lines():
+    batch, unallocated_line = make_batch_and_line("DECORATIVE-TRINKET", 20, 2)
+    batch.deallocate(unallocated_line)
+    assert batch.available_quantity == 20
+
+
+def test_allocation_is_idempotent():
+    batch, line = make_batch_and_line("ANGULAR-DESK", 20, 2)
+    batch.allocate(line)
+    batch.allocate(line)
+    assert batch.available_quantity == 18
+
+
+def test_equality():
+    assert Money('gbp', 10) == Money('gbp', 10)
+    assert Name('Harry', 'Percival') != Name('Bob', 'Gregory')
+    assert Line('RED-CHAIR', 5) == Line('RED-CHAIR', 5)
+
+
+def test_barry_is_harry():
+    harry = Person(Name("Harry", "Percival"))
+    barry = harry
+    barry.name = Name("Barry", "Percival")
+    assert harry is barry and barry is harry
