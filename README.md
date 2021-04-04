8ab34034463b5a84fc99fc0f53983af02082fabc
swill <swilltec@gmail.com>
2021-04-04 13:22:45 -0400

    set up project base

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# furniture
+# Python Furniture Project
+
+## This project is used to practice clean Python architecture
---
+++ model.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+from typing import Optional
+from datetime import date
+
+@dataclass(frozen=True)
+class OrderLine:
+    orderid: str
+    sku: str
+    qty: int
+
+class Batch:
+    def __init__(self, ref: str, sku: str, 
+        qty: int, eta:Optional[date]):
+    self.reference = ref
+    self.sku = sku
+    self.eta = eta
+    self.available_quantity = qty
+    def allocate(self, line: OrderLine):
+    self.available_quantity -= line.qty
