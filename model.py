4d043bb17b6edcc45b5f2d3d6355e275b6113110
swill <swilltec@gmail.com>
2021-04-04 13:37:09 -0400

    added check allocation permission

--- model.py
+++ model.py
@@ -2,21 +2,24 @@ from dataclasses import dataclass
 from typing import Optional
 from datetime import date
 
+
 @dataclass(frozen=True)
 class OrderLine:
     orderid: str
     sku: str
     qty: int
 
+
 class Batch:
-    def __init__(self, ref: str, sku: str, 
-        qty: int, eta:Optional[date]):
-    self.reference = ref
-    self.sku = sku
-    self.eta = eta
-    self.available_quantity = qty
-    def allocate(self, line: OrderLine):
-    self.available_quantity -= line.qty
+    def __init__(self, ref: str, sku: str,
+                 qty: int, eta: Optional[date]):
+        self.reference = ref
+        self.sku = sku
+        self.eta = eta
+        self.available_quantity = qty
 
     def allocate(self, line: OrderLine):
         self.available_quantity -= line.qty
+
+    def can_allocate(self, line: OrderLine) -> bool:
+       return self.sku == line.sku and self.available_quantity >= line.qty
